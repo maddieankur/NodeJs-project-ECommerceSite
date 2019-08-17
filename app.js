7c0981d8c41ee572a51ab536406af13d1233f86b
Ankur Kaintura <maddieankur@gmail.com>
2019-08-18 00:53:37 +0530

    Upload Folder & File

---
+++ app.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const http = require('http')
+const fs = require('fs');
+
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const app = express();
+
+app.set('view engine','pug');
+app.set('views','views');
+
+const adminData = require('./routes/admin.js');
+
+const shopRoutes = require('./routes/shop.js');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')))
+
+app.use('/admin',adminData.routes);
+app.use(shopRoutes);
+
+app.use((req,res,next) => {
+    res.status(404).render('404')
+})
+
+app.listen(3000);
